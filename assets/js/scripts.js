2efa0afeb13bfa233b127cd243aa5d8c685800b5
kevingsp <kevingsp@users.noreply.github.com>
2015-12-03 00:45:24 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -277,7 +277,7 @@ jQuery(function ($) {
     // -------------------------------------------------------------
 
     (function () {
-        var myLatlng = new google.maps.LatLng(41.372641, -74.687387);
+        var myLatlng = new google.maps.LatLng(40.761311, -73.8863887);
 
             var styles = [
                 {
