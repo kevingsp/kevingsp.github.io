c90546a1714501cee268cc640ac78982bc270ccf
kevingsp <kevingsp@users.noreply.github.com>
2015-12-03 00:49:33 -0500

    Update sendemail.php

--- assets/php/sendemail.php
+++ assets/php/sendemail.php
@@ -6,7 +6,7 @@
 	$message = @trim(stripslashes($_POST['message'])); 
 
 	$email_from = $email;
-	$email_to = 'nasirbinburhan@gmail.com'; //replace with your email
+	$email_to = 'kevingsp@gmail.com'; //replace with your email
 
 	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
@@ -17,6 +17,6 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
-	<script>alert("Thank you for contact us. As early as possible  we will contact you.");</script>
+	<script>alert("Thank you for contacting me. I'll get back to you soon.");</script>
 	<meta HTTP-EQUIV="REFRESH" content="0; url=http://trendytheme.net/demo/iamx/v/"> 
 </head>
